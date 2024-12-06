80ea50ca54f4e6b18e94d6cf977b981a69447a18
Andrew Wichmann <andrewp.wichmann@gmail.com>
2024-12-06 10:26:58 -0500

    add movement

--- day6/main.py
+++ day6/main.py
@@ -8,13 +8,14 @@ visited = "$"
 guard = "^"
 
 class Orientation(Enum):
-    UP = "up"
-    DOWN = "down"
-    RIGHT = "right"
-    LEFT = "left"
+    UP = 1
+    DOWN = 2
+    RIGHT = 3
+    LEFT = 4
 
 class Board():
     def __init__(self, puzzle_file):
+        self.visited_count = 0
         with open(puzzle_file, "r") as f:
             puzzle = f.readlines()
         self.board: T.List[T.List[str]] = [[0]*len(puzzle[0])]*len(puzzle)
@@ -23,7 +24,16 @@ class Board():
             for j, square in enumerate(row):
                 self.board[i][j] = square
                 if square == guard:
-                    self.guard_position = (i,j)
+                    self.guard_position = [i,j]
+
+    def move(self, x, y):
+        self.guard_position[0] += x
+        self.guard_position[1] += y
+        if self.board[self.guard_position[0]][self.guard_position[1]] != visited:
+            self.board[self.guard_position[0]][self.guard_position[1]] = visited
+            self.visited_count += 1
+
+
 
 class Guard():
     def __init__(self, board: Board):
@@ -31,7 +41,14 @@ class Guard():
         self.orientation = Orientation.UP
 
     def walk(self):
-        self.board
+        if self.orientation == Orientation.UP:
+            self.board.move(0,-1)
+        elif self.orientation == Orientation.DOWN:
+            self.board.move(0,1)
+        elif self.orientation == Orientation.RIGHT:
+            self.board.move(1,0)
+        elif self.orientation == Orientation.LEFT:
+            self.board.move(-1,0)
 
     def turn(self):
         pass
@@ -44,9 +61,16 @@ class Guard():
         self.board.guard_position
 
 board = Board(sys.argv[1])
+g = Guard(board)
 print(board.guard_position)
-g = Guard()
 g.walk()
+print(board.guard_position)
+g.orientation = Orientation.DOWN
+g.walk()
+print(board.guard_position)
+g.orientation = Orientation.RIGHT
+g.walk()
+print(board.guard_position)
 
 #while guard.next != None:
 #    if guard.next == obstacle:
